a5cc06257f7569cc3538a1b8c97ff328797f5980
oscarmontiel <etherealflux94@gmail.com>
2025-05-01 01:34:29 -0600

    +

--- ifs.sh
+++ ifs.sh
@@ -7,6 +7,14 @@
 echo "Your UID is ${UID}"
 # Dispay the username
 USER_NAME=$(id -un)
-echo "Your user name is ${USER_NAME}"
+echo "Your user name is ${USER_NAME}."
+echo "You don't need to know mine."
 # Display if the user is the root or not
+if [[ "${UID}" -eq 0 ]]
+then
+	echo 'You are alone.'
+else
+	echo 'You are not alone'
+fi
+
 
