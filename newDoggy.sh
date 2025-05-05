15e6e0fc4ca5bb40f03974e03e0700c395948db9
oscarmontiel <etherealflux94@gmail.com>
2025-05-04 23:21:37 -0600

    +

---
+++ newDoggy.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# This scripts creates an account on the local system
+# You will be prompted for the account name and password.
+
+# Ask for user name.
+echo "I am the Buddha. Who are you?"
+read -p "Who?: " USERNAME
+echo "WRONG! You are not ${USERNAME}..."
+echo "You are me."
+echo "And I am you."
+# Ask for the real name.
+
+# Ask for the password.
+
+# Create the user.
+
+# Set the password for the user.
+
+# Force password change on first login.#!/bin/bash
+
+# This scripts creates an account on the local system
+# You will be prompted for the account name and password.
+
+# Ask for user name.
+
+# Ask for the real name.
+
+# Ask for the password.
+
+# Create the user.
+
+# Set the password for the user.
+
+# Force password change on first login.#!/bin/bash
+
+# This scripts creates an account on the local system
+# You will be prompted for the account name and password.
+
+# Ask for user name.
+
+# Ask for the real name.
+
+# Ask for the password.
+
+# Create the user.
+
+# Set the password for the user.
+
+# Force password change on first login.
